57933f9dd6877b33b2928b4f9a1a9a7b8692ae14
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-21 15:31:03 +0200

    Created Setup Opengl Function.
    Here I initialized matrix mode, 2D orthographic projection and cleared the color buffer bit.

--- Triangle/Triangle.py
+++ Triangle/Triangle.py
@@ -18,3 +18,25 @@ def window_setup(): # Create a Function Called Window Setup.
 
     # Make this window the current context.
     glfw.make_context_current(window)
+
+
+def setup_opengl(): #Create a funtion to setup OpenGL
+
+    # Set the current matrix mode to GL_PROJECTION
+    glMatrixMode(GL_PROJECTION)
+    
+    # Load the identity matrix into the current matrix (initialize to identity)
+    glLoadIdentity()
+    
+    # Define a 2D orthographic projection matrix
+    # Parameters: left, right, bottom, top, near, far
+    glOrtho(-4, 4, -3, 3, -1, 1)
+    
+    # Set the current matrix mode to GL_MODELVIEW
+    glMatrixMode(GL_MODELVIEW)
+    
+    # Load the identity matrix into the current matrix (initialize to identity)
+    glLoadIdentity()
+    
+    # Clear the color buffer with the specified clear color
+    glClear(GL_COLOR_BUFFER_BIT)
