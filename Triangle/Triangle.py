f59bfe733d32c34f4b44346b6b07ca052ba27628
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-21 15:24:11 +0200

    Created a Window setup Function.
    Here I initialized Graphics library , Created Window screen and make the window current.

--- Triangle/Triangle.py
+++ Triangle/Triangle.py
@@ -2,3 +2,19 @@ import glfw
 from OpenGL.GL import *
 
 
+def window_setup(): # Create a Function Called Window Setup.
+
+    if not glfw.init():  # If Graphics Library Framework is not initialized, it will return None.
+        return None 
+    
+    glfw.window_hint(glfw.RESIZABLE, glfw.TRUE)  # This function allows the window size to be resizable.
+
+    window = glfw.create_window(800, 600, "TRIANGLE", None, None)  # Create the window with the specified width, height, and title.
+
+    # If the window creation fails, it will return None.
+    if not window:
+        glfw.terminate()
+        return None
+
+    # Make this window the current context.
+    glfw.make_context_current(window)
